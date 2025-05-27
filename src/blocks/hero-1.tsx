ed8cfd0c391b80a8d75b8b9dbfdfe8467dd2dc2d
Sil <silveltman@gmail.com>
2025-05-27 18:30:05 +0200

    editable

--- src/blocks/hero-1.tsx
+++ src/blocks/hero-1.tsx
@@ -4,13 +4,13 @@ export default function () {
   return (
     <section className="relative w-full py-16">
       <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center px-4 lg:px-8">
-        <span className="text-primary mb-4 items-center justify-center text-base font-medium">
+        <span className="text-primary editable mb-4 items-center justify-center text-base font-medium">
           Welkom hier!
         </span>
-        <h1 className="text-center text-6xl font-bold">
+        <h1 className="editable text-center text-6xl font-bold">
           Krijg inzicht door meditatie en coaching met Ank Schravendeel
         </h1>
-        <p className="mt-4 text-center text-xl">
+        <p className="editable mt-4 text-center text-xl">
           Meditatie: zijn met wat er is en dat toelaten. <br />
           Coaching: je ervaring geeft inzicht en je leert je er toe verhouden.
         </p>
