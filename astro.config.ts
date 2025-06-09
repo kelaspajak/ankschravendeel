1ac3e6290662213b9ccd66fc25310504b2fd6d58
kelaspajak <57969740+kelaspajak@users.noreply.github.com>
2025-06-09 11:12:59 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -52,4 +52,11 @@ export default defineConfig({
       compatibilityDate: "2024-05-01" // You can adjust this date as needed
     }
   }),
+
+  resolve: {
+  // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+  // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+  alias: import.meta.env.PROD ? {
+    "react-dom/server": "react-dom/server.edge",
+  } : undefined,
 })
